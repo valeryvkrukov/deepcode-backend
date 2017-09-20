f028bff1c04adc5daf48f95f41bdecd756d277e6
Valer Krukov <valery.v.krukov@gmail.com>
2017-09-20 03:54:55 +0300

    front build updated

--- src/AppBundle/Security/Guard/JWTTokenAuthenticator.php
+++ src/AppBundle/Security/Guard/JWTTokenAuthenticator.php
@@ -1,10 +1,17 @@
 <?php
 namespace AppBundle\Security\Guard;
 
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Security\Guard\JWTTokenAuthenticator as BaseAuthenticator;
 
 class JWTTokenAuthenticator extends BaseAuthenticator
 {
+	public function checkCredentials($credentials, UserInterface $user)
+    {
+    	return false;
+    	//var_dump($user);die();
+        return true;
+    }
 }
