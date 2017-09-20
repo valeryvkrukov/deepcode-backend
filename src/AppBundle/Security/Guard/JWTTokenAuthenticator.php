9e43fb45981a43b47de86a27c27d456dc82f7c21
Valer Krukov <valery.v.krukov@gmail.com>
2017-09-20 04:13:29 +0300

    access for customers denied

--- src/AppBundle/Security/Guard/JWTTokenAuthenticator.php
+++ src/AppBundle/Security/Guard/JWTTokenAuthenticator.php
@@ -2,16 +2,22 @@
 namespace AppBundle\Security\Guard;
 
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\InvalidPayloadException;
+use Lexik\Bundle\JWTAuthenticationBundle\Exception\UserNotFoundException;
+use Lexik\Bundle\JWTAuthenticationBundle\Security\Authentication\Token\PreAuthenticationJWTUserToken;
 
 use Lexik\Bundle\JWTAuthenticationBundle\Security\Guard\JWTTokenAuthenticator as BaseAuthenticator;
 
 class JWTTokenAuthenticator extends BaseAuthenticator
 {
 	public function checkCredentials($credentials, UserInterface $user)
-    {
-    	return false;
-    	//var_dump($user);die();
-        return true;
+	{
+		if ($user->hasRole('ROLE_ADMIN')) {
+            return true;
+        }
+        return false;
     }
 }
